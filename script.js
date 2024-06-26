a03b83dca259779612a51f1fe622bd1f19b326c9
WeslingDeJesus <158292040+WeslingDeJesus@users.noreply.github.com>
2024-06-26 19:35:53 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function Togglemenu(){
+    const menuToggle = document.querySelector('.menuToggle');
+    menuToggle.classList.toggle("active");
+    const navigation = document.querySelector('.nav_container');
+    navigation.classList.toggle("active");
+}
